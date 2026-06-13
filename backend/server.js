89836c82118e508174f82489524c2110ff2a100c
RitualLizard710 <163294847+RitualLizard710@users.noreply.github.com>
2026-06-13 14:33:29 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,19 @@
+require("dotenv").config();
 
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("API do Sistema de Reforço Escolar");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
