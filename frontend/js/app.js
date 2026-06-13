d5fa0f4c0ea71860048966a77a356b9b31edb62a
RitualLizard710 <163294847+RitualLizard710@users.noreply.github.com>
2026-06-13 14:44:34 -0300

    Update app.js

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,1 +1,35 @@
-console.log("Sistema carregado!");
+function entrar() {
+
+    document
+        .getElementById("telaLogin")
+        .classList.add("oculto");
+
+    document
+        .getElementById("aplicacao")
+        .classList.remove("oculto");
+
+}
+
+function abrirRegistro() {
+
+    document
+        .getElementById("telaLogin")
+        .classList.add("oculto");
+
+    document
+        .getElementById("telaRegistro")
+        .classList.remove("oculto");
+
+}
+
+function voltarLogin() {
+
+    document
+        .getElementById("telaRegistro")
+        .classList.add("oculto");
+
+    document
+        .getElementById("telaLogin")
+        .classList.remove("oculto");
+
+}
